010ab9d61d133673c77a93416e562da4f217d17f
9eko <remonskash@gmail.com>
2020-01-30 13:06:48 +0900

    Append to previous commit

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,22 @@
 $(function() {
 
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `
+  }
+
+  function addNoUser() {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `
+  }
+
   $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
     $.ajax({
@@ -16,9 +33,9 @@ $(function() {
           addUser(user);
         });
       } else if (input.length == 0) {
-        retun false;
+        return false;
       } else {
-        addUser();
+        addNoUser();
       }
     })
     .fail(function() {
